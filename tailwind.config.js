fca93ff9152080426f98de65dc169c0406ad0476
itarek99 <tarekulislam7060@gmail.com>
2023-06-11 00:48:26 +0600

    container edited

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ export default {
     fontFamily: {
       sans: ['Roboto', 'sans-serif'],
     },
+    container: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
+    },
     extend: {
       colors: {
         primary: {
