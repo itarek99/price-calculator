ae80d201083145e3fc427312c3b623b1f3fb1d0c
itarek99 <tarekulislam7060@gmail.com>
2023-06-17 23:37:21 +0600

    loading animation border fixed

--- src/Pages/PricePage/Component/SelectDifficulty.jsx
+++ src/Pages/PricePage/Component/SelectDifficulty.jsx
@@ -124,7 +124,7 @@ const SelectDifficulty = ({
                 {imageLoaded ? null : (
                   <div style={{ height: loaderHeight + 'px' }} className='flex items-center justify-center'>
                     <div
-                      className='inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-primary border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]'
+                      className='inline-block h-12 w-12 animate-spin rounded-full border-2 border-solid border-primary border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]'
                       role='status'
                     ></div>
                   </div>
